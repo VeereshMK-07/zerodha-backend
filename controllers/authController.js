120568a3bfd3714c5b2c8f6e17291045d0116e3a
Veeresh Kakamari <veereshkakamri@gmail.com>
2026-03-16 20:37:16 +0530

    fixed the send otp

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,19 +2,21 @@ const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
 exports.sendOtp = async (req, res) => {
-
   try {
 
     const { phone } = req.body;
 
-    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    if (!phone) {
+      return res.status(400).json({ message: "Phone required" });
+    }
 
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
     const otpExpiry = new Date(Date.now() + 5 * 60 * 1000);
 
     let user = await User.findOne({ phone });
 
     if (!user) {
-      user = await User.create({ phone });
+      user = new User({ phone });
     }
 
     user.otp = otp;
@@ -27,16 +29,13 @@ exports.sendOtp = async (req, res) => {
     res.json({ message: "OTP sent successfully" });
 
   } catch (err) {
+    console.error("SEND OTP ERROR:", err);
     res.status(500).json({ message: "Server error" });
   }
-
 };
 
-
 exports.verifyOtp = async (req, res) => {
-
   try {
-
     const { phone, otp } = req.body;
 
     const user = await User.findOne({ phone });
@@ -45,26 +44,22 @@ exports.verifyOtp = async (req, res) => {
       return res.status(400).json({ message: "Invalid or expired OTP" });
     }
 
-    const token = jwt.sign(
-      { id: user._id },
-      process.env.JWT_SECRET,
-      { expiresIn: "7d" }
-    );
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
 
     res.cookie("token", token, {
       httpOnly: true,
       secure: false,
-      sameSite: "lax"
+      sameSite: "lax",
     });
 
     res.json({ message: "Login successful" });
-
   } catch (err) {
+    console.error("SEND OTP ERROR:", err);
     res.status(500).json({ message: "Server error" });
   }
-
 };
-
 
 exports.logout = (req, res) => {
   res.clearCookie("token");
