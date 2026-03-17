8887a268c253f3677899cd99b3e1f773de8d2832
Veeresh Kakamari <veereshkakamri@gmail.com>
2026-03-17 14:16:24 +0530

    fixed authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,4 +6,13 @@ router.post("/send-otp", sendOtp);
 router.post("/verify-otp", verifyOtp);
 router.post("/logout", logout);
 
+
+
+router.get("/check-auth", (req, res) => {
+  if (req.cookies.token) {
+    return res.status(200).json({ success: true });
+  }
+  return res.status(401).json({ success: false });
+});
+
 module.exports = router;
