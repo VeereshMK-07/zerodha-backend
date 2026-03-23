51e2c8040641c7c9f5ac56498c3b7e33b9055bad
Veeresh Kakamari <veereshkakamri@gmail.com>
2026-03-23 21:16:13 +0530

    Added username

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { sendOtp, verifyOtp, verifyUser, logout } = require("../controllers/authController");
+const { sendOtp, verifyOtp, verifyUser, logout, saveName } = require("../controllers/authController");
 
 router.post("/send-otp", sendOtp);
 router.post("/verify-otp", verifyOtp);
